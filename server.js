a8ec87b53869f46fec017cb6ff5038c663342246
Neo Fiesta <newytpremium001@gmail.com>
2024-04-26 13:00:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import fetch from 'node-fetch';
+
+const app = express()
+
+async function findKey(url) {
+    try {
+        console.log("Finding Keys for ", url)
+        url = `https://extractapi.xyz/drm.php?v=${url}`
+        const response = await fetch(url, {
+            method: "GET"
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json(); 
+        console.log("Got Keys ", data.keys[0])
+        return data.keys[0]; 
+    } catch (error) {
+        throw new Error('Error:', error);
+    }
+}
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/pw', async function (req, res) {
+    const videoUrl = req.query.videourl;
+    const data = await findKey(videoUrl);
+  res.send(data)
+})
+
+// keyFinder.js
+
+
+
+app.listen(3000)
